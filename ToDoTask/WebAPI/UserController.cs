fdffbe12063fcad4d1af116dac4fb453c166e744
VladislavSoltanov <vsoltanovv@gmail.com>
2022-06-29 11:12:38 +0200

    Начал создавать WebApi для регистрации пользователя

---
+++ ToDoTask/WebAPI/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoTask.Models;
+
+namespace ToDoTask.WebAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        ApplicationContext db; 
+
+        private readonly ILogger<UserController> _logger;
+
+        [HttpPost]
+        public async Task<IActionResult> Createuser([FromBody] User user)
+        {
+           db.User.Where(u => u.Id == user.Id);
+        }
+    }
+}
