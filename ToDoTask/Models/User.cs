8a67fc658d0a33c94fdc030451436b208588f8ab
VladislavSoltanov <vsoltanovv@gmail.com>
2022-06-30 12:44:16 +0200

    Мелкие корректировки

--- ToDoTask/Models/User.cs
+++ ToDoTask/Models/User.cs
@@ -8,7 +8,7 @@
         /// <summary>
         /// Идентификатор
         /// </summary>
-        public int Id { get; set; }
+        public int UserId { get; set; }
 
         /// <summary>
         /// Имя
