aebee98e135cbbb803ea3290b46e3e742844e086
VladislavSoltanov <vsoltanovv@gmail.com>
2022-07-03 08:49:18 +0200

    Добавление WebApi  для вывода задачи, для создания задачи

--- ToDoTask/Controllers/TaskController.cs
+++ ToDoTask/Controllers/TaskController.cs
@@ -2,33 +2,49 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoTask.Models;
 using ToDoTask.Models.Task;
-using ToDoTaskServer.Models.ViewModes;
+using ToDoTaskServer.Models.ViewModel;
 
 namespace ToDoTaskServer.Controllers
 {
     [Route("api/TaskController")]
     public class TaskController : Controller
     {
-        private ApplicationContext _db;
         private readonly ILogger<TaskController> _logger;
+        private ApplicationContext _db;
 
         public TaskController(ApplicationContext db, ILogger<TaskController> logger)
         {
             _db = db;
             _logger = logger;
         }
+        [Route("CreateTask")]
+        [HttpGet]
+        public async Task<IActionResult> ViewAllTask()
+        {
+            try
+            {
+                _logger.LogInformation("Запрос получен");
+                return Ok(_db.TodoTask);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest(_logger);
+            }
+        }
+
 
         [Route("CreateTask")]
         [HttpPost]
-        public async Task<ActionResult<Task>> ViewTask([FromBody] TaskViewModel model)
+        public async Task<ActionResult<TodoTask>> CreateTask([FromBody] TaskViewModel model)
         {
             try
             {
                 _logger.LogInformation("Запрос получен");
 
-                var config = new MapperConfiguration(cfg => cfg.CreateMap<TaskViewModel, Task>());
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<TaskViewModel, TodoTask>());
                 var mapper = new Mapper(config);
-                var result = mapper.Map<Task>(model);
+                var result = mapper.Map<TodoTask>(model);
 
                 _db.Add(result);
                 _db.SaveChanges();
