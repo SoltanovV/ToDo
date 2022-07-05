fdda984f250d9a94c9e9664212efa6ad9ea7bd6c
VladislavSoltanov <vsoltanovv@gmail.com>
2022-07-05 20:36:31 +0200

    Изменение архитектуры приложения: Создание новых промежуточных моделей

---
+++ ToDoTask/Models/ViewModel/TodoViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace ToDoTaskServer.Models.ViewModel
+{
+    public class TodoViewModel
+    {
+        /// <summary>
+        /// Название задачи
+        /// </summary>
+        public string NameTask { get; set; }
+
+        /// <summary>
+        /// Описание задачи
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Дата постановки задачи
+        /// </summary>
+        public DateTime StartData { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// Дата завершения задачи
+        /// </summary>
+        public DateTime EndData { get; set; }
+    }
+}
