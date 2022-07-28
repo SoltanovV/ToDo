96c0c0e318b3c3e649716bd6df5c6ec686537fdd
VladislavSoltanov <vsoltanovv@gmail.com>
2022-07-28 12:47:27 +0200

    настройка react

--- reactproject11/src/index.js
+++ reactproject11/src/index.js
@@ -7,6 +7,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+        <App>
+            <button id="btn" value="Запрос">Запрос</button>
+        </App>
   </React.StrictMode>
 );
