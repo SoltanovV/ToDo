ee69687c73b5b5ebb14a8e0a74ab2deb110cf499
K0Ma19 <korunets1901@gmail.com>
2022-08-12 19:49:20 +0300

    Настройка подключения к бд

--- ToDoTask/Models/ApplicationContext.cs
+++ ToDoTask/Models/ApplicationContext.cs
@@ -7,12 +7,12 @@ namespace ToDoTask.Models
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<Project> Project { get; set; }
-        public DbSet<Account> Account { get; set; }
-        public DbSet<User> User { get; set; }
-        public DbSet<Todo> Todo { get; set; }
-        public DbSet<Priority> Priority { get; set; }
-        public DbSet<Status> Status { get; set; }
+        public DbSet<Project> Project { get; set; } = null!;
+        public DbSet<Account> Account { get; set; } = null!;
+        public DbSet<User> User { get; set; } = null!;
+        public DbSet<Todo> Todo { get; set; } = null!;
+        public DbSet<Priority> Priority { get; set; } = null!;
+        public DbSet<Status> Status { get; set; } = null!;
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
