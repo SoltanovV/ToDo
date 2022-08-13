86e7856455a732725c6a9db935e47c2aba9aa822
VladislavSoltanov <vsoltanovv@gmail.com>
2022-08-13 12:35:46 +0200

    Реализовал создание карточек с информацией

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -20,8 +20,6 @@ export default class App extends Component {
                         items: result
                     });
                 },
-                // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
-                // чтобы не перехватывать исключения из ошибок в самих компонентах.
                 (error) => {
                     this.setState({
                         isLoaded: true,
@@ -32,43 +30,23 @@ export default class App extends Component {
     }
     render() {
         const { error, isLoaded, items } = this.state;
-        const project = items
-        let pro
-        let text
-        //console.log(items)
 
-        //Point
-// for (let i = 0; i < comment.length; i++) {
-//     return comm = comment[i].comment.$values
-//     Comment
-//     console.log(comment[i].x)
-//     for (let j = 0; j < comm.length; j++) {
-//         text = comm[i].text
-//         //console.log(text)
-//     }
-// }
-//         for(let i=0; i < project.length; i++){
-//             pro = project[i].projects
-//             for(let j=0; j < pro.length; j++){
-//                 text = pro[i]
-//                 console.log(text.name)
-//                 console.log(text.deadLine)
-//             }
-//         }
-        console.log(items[2])
         if (error) {
             return <div>Ошибка: {error.message}</div>;
         } else if (!isLoaded) {
             return <div>Загрузка...</div>;
         } else {
             return (
-                <ul>
-                    {items.map(item => (
-                        <li key={item.id}>
-
-                        </li>
-                    ))}
-                </ul>
+               <div>
+                   {items.map(item =>(
+                       <div key={item.id}>
+                           <h1> {item.projects.map(p =>(p.name))}</h1>
+                           <h3>Задачи: {item.todos.map(p =>(p.nameTask))}</h3>
+                           <h3> Выполняющий: {item.name}</h3>
+                           <h3> </h3>
+                       </div>
+                   ))}
+               </div>
             );
         }
     }
