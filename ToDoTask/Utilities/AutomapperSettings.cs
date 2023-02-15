f7fd2ae8a3bd2ad25bd25bfcd120f29955f7ef22
Vladislav <vsoltanovv@gmail.com>
2023-02-15 23:06:25 +0200

    Изменение работы автомаппера

--- ToDoTask/Utilities/AutomapperSettings.cs
+++ ToDoTask/Utilities/AutomapperSettings.cs
@@ -19,14 +19,35 @@ namespace AspBackend.Utilities
                 config.CreateMap<UserRequest, User>().ReverseMap();
                 config.CreateMap<UserResponce, User>().ReverseMap();
 
-                //config.CreateMap<CreatePersonResponce, Person>();
-                //config.CreateMap<Person, CreatePersonResponce>();
+                config.CreateMap<SigInRequest, User>().ReverseMap();
+                config.CreateMap<SigInResponce, User>().ReverseMap();
 
-                //config.CreateMap<SearchPersonRequest, Person>();
-                //config.CreateMap<Person, SearchPersonRequest>();
+                config.CreateMap<SigInRequest, Account>().ReverseMap();
+                config.CreateMap<SigInResponce, Account>().ReverseMap();
 
-                //config.CreateMap<SearchPersonResponce, Person>();
-                //config.CreateMap<Person, SearchPersonResponce>();
+                #region Маппинг для создания задачи
+                config.CreateMap<CreateTodoRequest, Todo>().ReverseMap();
+                config.CreateMap<CreateTodoResponce, Todo>().ReverseMap();
+
+                config.CreateMap<CreateTodoRequest, Status>().ReverseMap();
+                config.CreateMap<CreateTodoResponce, Status>().ReverseMap();
+
+                config.CreateMap<CreateTodoRequest, Priority>().ReverseMap();
+                config.CreateMap<CreateTodoResponce, Priority>().ReverseMap();
+                #endregion
+
+                #region Маппинг для обновления задачи
+                config.CreateMap<UpdateTodoRequest, Todo>().ReverseMap();
+                config.CreateMap<UpdateTodoResponce, Todo>().ReverseMap();
+
+                //config.CreateMap<UpdateTodoRequest, Status>().ReverseMap();
+                //config.CreateMap<UpdateTodoResponce, Status>().ReverseMap();
+
+                //config.CreateMap<UpdateTodoRequest, Priority>().ReverseMap();
+                //config.CreateMap<UpdateTodoResponce, Priority>().ReverseMap();
+                #endregion
+
+
             });
 
             return mapperConfig;
