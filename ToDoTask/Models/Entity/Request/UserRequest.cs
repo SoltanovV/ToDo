80afd8f4cfdfd359c1f326e7f627110dbec3f62b
Vladislav <vsoltanovv@gmail.com>
2023-02-15 23:04:59 +0200

    Добавление моделей для получения запросов

--- ToDoTask/Models/Entity/Request/UserRequest.cs
+++ ToDoTask/Models/Entity/Request/UserRequest.cs
@@ -9,12 +9,12 @@ public class UserRequest
     /// <summary>
     /// Имя пользователя
     /// </summary>
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
     /// <summary>
     /// Email аккаунта
     /// </summary>
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
 
 }
